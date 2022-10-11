fbd1f16b147f8ab067b3f3d793cd50e5417eb21d
ZaccJacc <zaclijackson@gmail.com>
2022-10-11 22:15:16 +0100

    Added some comments to zombie, and planning to turn the mertrics into interfaces, because they will behave better if i do. Not trying to make an object as something, just want to be able to use some default manipulations.

--- src/Health.java
+++ src/Health.java
@@ -1,6 +1,6 @@
-public class Health {
+public class Health { //ok for very long reasons, i'm going to turn this into an interface because i can and it'll work better
     public double health;
-    private int strength;
+    private final int strength;
 
     public Health(int strength){
         this.health = strength*10;//currently based the health off multiplying the level by 10, this is up for change
@@ -18,7 +18,7 @@ public class Health {
 
     }
 
-    public void raiseHealth(int healthValue){
+    public void raiseHealth(int healthValue){ //can be used for both eating and healing packages.
         if ((this.health + healthValue*6)>this.strength*10){
             this.health = this.strength*10;
         } else {
--- src/Zombie.java
+++ src/Zombie.java
@@ -12,10 +12,12 @@ public class Zombie extends Health{
         this.strength = strength;
         this.health = new Health(this.strength);
 
-        /*TODO: Health metric
-        *       Attack Value Metric
+        /*
+        *   TODO: Health metric ##
+        *       Attack Value Metric ## Needs extension, premise is there.
         *       A method for registering hits, and their relative damage. Could be done through an attack class
-        *           Maybe apply in health? Or maybe have health as an attribute of a wider conflict file*/
+        *           -> Maybe apply in health? Or maybe have health as an attribute of a wider conflict file
+        */
     }
 
 
@@ -23,15 +25,11 @@ public class Zombie extends Health{
 
 
     public static void main(String[] args){
-        Keypress press = new Keypress();
+        Keypress press = new Keypress();//got some questions over the implementation of an event through an object?
+        // wouldn't the event need to be a direct attribute associated with the object that will manage it? And, by extension, would it not function incorrectly if we try and force it into another object?
+        // I say the above because I have no idea how that event actually works.
         Zombie zombie = new Zombie(1); //Solely for testing, this will form the basis of the opponent zombie object
-        /*while (zombie.health.health > 0){
-            if (zombie.health.logHit(zombie.strength)){
-                System.out.println("dead get good lmfao");
-            } else {
-                System.out.println("didnt die");
-            }
-        }*/
+
         System.out.println(zombie.health.health);
 
     }
