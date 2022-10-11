7b3a27740980f588b270dfc1c9075037a956880e
ZaccJacc <zaclijackson@gmail.com>
2022-10-11 14:15:44 +0100

    googoogagag

--- src/Health.java
+++ src/Health.java
@@ -1,7 +1,18 @@
 public class Health {
     public double health;
 
+    public Health(int strength){
+        this.health = strength*10;//currently based the health off multiplying the level by 10, this is up for change
+    }
+
     public boolean logHit(int severity){
-        return true;
+        if (this.health <= severity*5){ //will it die
+            return true;
+        }
+        else {
+            this.health = this.health - severity*5; //if it aint gonna die, knock the health off and return false
+            return false;
+        }
+
     }
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,12 +1,14 @@
 import java.util.*;
 import java.lang.*;
 
-public class Zombie {
-    public double
-    public double health;
-    public int strength;
-    public Zombie(){
-        this.health
+public class Zombie extends Health{
+    public Health health;
+    public int strength; //metric from 1-5? Health can be calculated based off this and the attack severity based off that?
+    public Zombie(int strength){
+        super(strength);
+        this.strength = strength;
+        this.health = new Health(this.strength);
+
         /*TODO: Health metric
         *       Attack Value Metric
         *       A method for registering hits, and their relative damage. Could be done through an attack class
@@ -15,7 +17,18 @@ public class Zombie {
 
 
 
+
+
     public static void main(String[] args){
-        Zombie zombie = new Zombie(); //Solely for testing, this will form the basis of the opponent zombie object
+        Zombie zombie = new Zombie(1); //Solely for testing, this will form the basis of the opponent zombie object
+        /*while (zombie.health.health > 0){
+            if (zombie.health.logHit(zombie.strength)){
+                System.out.println("dead get good lmfao");
+            } else {
+                System.out.println("didnt die");
+            }
+        }*/
+        System.out.println(zombie.health.health);
+
     }
 }
