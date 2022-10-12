35b1163c30f1dcbcb89f916ddf2390f628d5afe8
ZaccJacc <58274381+ZaccJacc@users.noreply.github.com>
2022-10-12 22:00:58 +0100

    JavaDoc!!!
    Wooh!!! added javadoc summaries so people can actually see what's going on a lot easier (not that any of this is utility so why would they need to be able to wide-scale implement anything lmao)

--- src/Metric/Health.java
+++ src/Metric/Health.java
@@ -1,6 +1,12 @@
 package Metric;
 
-public interface Health { //ok for very long reasons, i'm going to turn this into an interface because i can and it'll work better
+/**
+ * Interface used to register and govern the default metric for keeping the entity alive. Extended by hunger,
+ * implemented by all entities and resources that can manipulate health.
+ * @see Hunger
+ * @see Zombie
+ */
+public interface Health {
 
     int getMetric(int metric);
     void setMetric(int metric, int value);
@@ -9,6 +15,13 @@ public interface Health { //ok for very long reasons, i'm going to turn this int
     int STRENGTH = 2;
 
 
+    /**
+     * The default method for logging a hit on the entity. This can also be done by using raiseHealth with a negative
+     * healthValue, but that does not account for the impacts of the attacking entities' strength.
+     * @param severity The severity of the hit - based on the strength of the entity attacking.
+     * @return A boolean value indicating whether the entity being attacked will die or not.
+     * @see #raiseHealth
+     */
     default boolean logHit(int severity){
         if (this.getMetric(HEALTH) <= severity*5){ //will it die
             return true;
@@ -20,6 +33,11 @@ public interface Health { //ok for very long reasons, i'm going to turn this int
 
     }
 
+    /**
+     * The default method of manipulating the entity's health metric. Can be used as an alternative to logHit.
+     * @param healthValue A factor representing the healing value of the resource being processed.
+     * @see #logHit
+     */
     default void raiseHealth(int healthValue){ //can be used for both eating and healing packages.
         if ((this.getMetric(HEALTH) + healthValue*6)>this.getMetric(STRENGTH)*10){
             this.setMetric(HEALTH, this.getMetric(STRENGTH)*10);
--- src/Zombie.java
+++ src/Zombie.java
@@ -7,21 +7,23 @@ import java.lang.*;
 
 public class Zombie implements Health, Hunger {
     public int health;
-    public int strength; //metric from 1-5? Metric.Health can be calculated based off this and the attack severity based off that?
-    public int hunger;
+    public int strength; //metric from 1-5? Metric. Health can be calculated based off this and the attack severity based off that?
+    public int hunger = 100; //decided to make this a percentage based thing.
 
     public Zombie(int strength){
         this.strength = strength;
         this.health = strength*10;
-
-        /*
-        *   TODO: Metric.Health metric ##
-        *       Attack Value Metric ## Needs extension, premise is there.
-        *       A method for registering hits, and their relative damage. Could be done through an attack class
-        *           -> Maybe apply in health? Or maybe have health as an attribute of a wider conflict file
-        */
     }
 
+
+    /**
+     * Returns the target metric for each category applicable to this entity.
+     * @param metric An integer to reference which of the object's metrics should be edited. Constants are implemented
+     *               through the interface Health.
+     * @return The current object's metric for the target passed as a parameter.
+     * @see Health
+     * @see #setMetric
+     */
     @Override
     public int getMetric(int metric){
         switch (metric) {
@@ -36,6 +38,14 @@ public class Zombie implements Health, Hunger {
         }
     }
 
+    /**
+     * Sets the target metric for each category applicable to this entity.
+     * @param metric An integer to reference which of the object's metrics should be edited. Constants are implemented
+     *               through the interface Health.
+     * @param value The value by which the metric should be updated.
+     * @see Health
+     * @see #getMetric
+     */
     @Override
     public void setMetric(int metric, int value){
         switch(metric){
