376cab0b9e680b7735f4fc0099912118dbf01c92
PalitXD <73961134+PalitXD@users.noreply.github.com>
2022-10-17 11:44:48 +0100

    e

--- src/Render/Renderer.java
+++ src/Render/Renderer.java
@@ -10,16 +10,31 @@ import java.io.InputStream;
 
 
 public class Renderer {
-
+public static int i = 0;
+public static boolean y = false;
     public static void Renderer() {
+
         GL11.glBegin(GL11.GL_LINES);
         GL11.glColor3f(0.4f, 0.0f, 0.4f);
         GL11.glVertex2f(100, 100);
-        GL11.glVertex2f(200, 400);
+        GL11.glVertex2f(200, 400+ i);
         GL11.glEnd();
         GL11.glBegin(GL11.GL_LINES);
         GL11.glColor3f(0.4f, 0.0f, 0.4f);
         GL11.glVertex2f(1180,100);
-        GL11.glVertex2f(1080, 400);
+        GL11.glVertex2f(1080, 400+ i);
+        System.out.println(i);
+        if(y == false){
+            i++;
+        }else{
+            i--;
+        }
+
+        if(i > 250){
+            y = true;
+        }else if(i <= 0){
+            y = false;
+        }
+
     }
 }
